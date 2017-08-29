7d748be7fc5e0ea8b17333516ad1cfab85e2a4bc
Brian Port <bport79@yahoo.com>
2017-08-29 15:54:34 -0400

    moved and fixed save, cleaned up a bit

--- TextEditorC/Form1.cs
+++ TextEditorC/Form1.cs
@@ -30,9 +30,10 @@ namespace TextEditorC
 
         private void contentsToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            //Program Info
             MessageBox.Show("Text Editor in C#." +
                 "\nBrian Port" +
-                "\nVersion: 1.00" +
+                "\nVersion: 1.04" +    //update as needed
                 "\nCoding Boot Camp");
         }
 
@@ -58,12 +59,13 @@ namespace TextEditorC
 
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            //open file function
             OpenFileDialog ofd = new OpenFileDialog();
             ofd.Filter = "Text Files (.txt)|*.txt";
             ofd.Title = "Open a file...";
-            if (ofd.ShowDialog() == DialogResult.OK);
+            if (ofd.ShowDialog() == DialogResult.OK)
             {
-                System.IO.StreamReader sr = new System.IO.StreamReader(ofd.FileName);
+                StreamReader sr = new StreamReader(ofd.FileName);
                 richTextBox1.Text = sr.ReadToEnd();
                 sr.Close();
             }
@@ -81,20 +83,13 @@ namespace TextEditorC
 
         private void saveToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            //Save Function
-            SaveFileDialog sfd = new SaveFileDialog();
-            sfd.Filter = "Text Files (.txt)|*txt";
-            sfd.Title = "Save file...";
-            if (sfd.ShowDialog() == DialogResult.OK) ;
-            {
-                System.IO.StreamWriter sw = new System.IO.StreamWriter(sfd.FileName);
-                sw.Write(richTextBox1.Text);
-                sw.Close();
-            }
+           
         }
 
         private void fontToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            //change font dialog
+            //show color option in font dialog
             fontDialog1.ShowColor = true;
 
             if (fontDialog1.ShowDialog() == DialogResult.OK)
@@ -104,8 +99,7 @@ namespace TextEditorC
 
         private void fontColorToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (colorDialog1.ShowDialog() == DialogResult.OK)
-                richTextBox1.ForeColor = colorDialog1.Color;
+            
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
@@ -129,6 +123,20 @@ namespace TextEditorC
                     richTextBox1.WordWrap = true;
                     richTextBox1.ScrollBars = RichTextBoxScrollBars.Vertical;
                     break;
+            }
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Save Function
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Text Files (.txt)|*.txt";
+            sfd.Title = "Save file as...";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter sw = new StreamWriter(sfd.FileName);
+                sw.Write(richTextBox1.Text);
+                sw.Close();
             }
         }
     }
